334b03e18ee597ed5270b18ca24650e0e25639f9
guilherme chaves <chaves.guilherme@proton.me>
2025-01-24 19:09:15 -0300

    chore: add fish

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,19 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set fish_greeting ""
+
+# theme
+set -g theme_color_scheme terminal-dark
+set -g fish_prompt_pwd_dir_length 1
+set -g theme_display_user yes
+set -g theme_hide_hostname no
+set -g theme_hostname always
+
+# aliases
+command -qv nvim && alias vim nvim
+
+set -gx EDITOR nvim
+
+source ~/.asdf/asdf.fish
