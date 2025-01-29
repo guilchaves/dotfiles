4e9bf32360e6d7a0d215528322b2c2a15c59b492
guilherme chaves <chaves.guilherme@proton.me>
2025-01-29 17:28:33 -0300

    chore: changing goimports for goline

--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -9,7 +9,7 @@ return {
                 null_ls.builtins.formatting.prettierd,
                 null_ls.builtins.formatting.black,
                 null_ls.builtins.formatting.isort,
-                null_ls.builtins.formatting.goimports,
+                null_ls.builtins.formatting.golines,
             },
         })
     end,
