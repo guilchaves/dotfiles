2b3c4123269cb1fe81702b80c5f3962332770be3
guilherme chaves <chaves.guilherme@proton.me>
2025-01-29 16:50:01 -0300

    chore: update formatting for go using goimports

--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -9,8 +9,7 @@ return {
                 null_ls.builtins.formatting.prettierd,
                 null_ls.builtins.formatting.black,
                 null_ls.builtins.formatting.isort,
-                null_ls.builtins.formatting.rubocop,
-                null_ls.builtins.diagnostics.rubocop,
+                null_ls.builtins.formatting.goimports,
             },
         })
     end,
