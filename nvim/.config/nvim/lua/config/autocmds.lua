0c44484d67887586804413a7ac1af7cb05b1d974
guilherme chaves <chaves.guilherme@proton.me>
2026-08-04 14:58:43 -0300

    refactor(nvim): consolida filetype plugins e autocmds
    - Aplica indentação de 2 espaços para linguagens web via único FileType autocmd em config/autocmds.lua
    - Mantém filetype plugins apenas para exceções: Go, Elixir, Python, Rust, Templ
    - Move snippet de tratamento de erro do Go para ftplugin/go.lua como keymap buffer-local
    - Remove util/go.lua e filetype plugins duplicados de css, html, htmx, javascript, javascriptreact, scss, typescript, typescriptreact, prisma e ruby

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -1,1 +1,21 @@
-require("util.go")
+local web_filetypes = {
+	"css",
+	"html",
+	"htmx",
+	"javascript",
+	"javascriptreact",
+	"scss",
+	"typescript",
+	"typescriptreact",
+	"prisma",
+	"ruby",
+}
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = web_filetypes,
+	callback = function()
+		vim.opt_local.tabstop = 2
+		vim.opt_local.softtabstop = 2
+		vim.opt_local.shiftwidth = 2
+	end,
+})
